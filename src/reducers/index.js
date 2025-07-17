ec419c5d4abc2d5f40a2c4df9d97e418822c26a2
KennyBryan1233 <131802055+KennyBryan1233@users.noreply.github.com>
2025-07-17 20:03:20 +0530

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import balanceReducer from './balanceReducer';
-
-const rootReducer = combineReducers({
-  balance: balanceReducer,
-});
-
-export default rootReducer;
--- src/utils/stockPriceSimulator.js
+++
@@ -1,6 +0,0 @@
-// utils/stockPriceSimulator.js
-export const generateRandomStockPrice = (currentPrice) => {
-  const volatility = 0.02; // 2% volatility
-  const changePercent = 2 * volatility * Math.random() - volatility;
-  return currentPrice * (1 + changePercent);
-};
